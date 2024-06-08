cf7bf669f9867f1cfd701573803108e66431086a
song-ari <sari0708@ajou.ac.kr>
2024-06-08 17:04:42 +0900

    feat: 가격에 포맷 함수 적용

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,13 @@ import heartIcon from "../assets/ic_heart.svg";
 export default function Card({ product = {} }) {
   const { images = [], name = "미제", price = 0, favoriteCount = 0 } = product;
 
+  const formatPrice = (price) => price.toLocaleString();
+
   return (
     <div className={styles.card}>
       <img src={images} alt="상품 이미지" className={styles.image} />
       <p className={styles.name}>{name}</p>
-      <span className={styles.price}>{price}원</span>
+      <span className={styles.price}>{formatPrice(price)}원</span>
       <span className={styles.heart}>
         <img src={heartIcon} alt="좋아요 이모티콘" />
         {favoriteCount}
